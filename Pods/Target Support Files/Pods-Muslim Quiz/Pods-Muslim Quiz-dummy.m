410afa2e85da132018a69d17c54f94928076a423
Amin Benarieb <amin.benarieb.work@gmail.com>
2018-10-24 22:08:33 +0300

    Pods deintegrated.

--- Pods/Target Support Files/Pods-Muslim Quiz/Pods-Muslim Quiz-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Muslim_Quiz : NSObject
-@end
-@implementation PodsDummy_Pods_Muslim_Quiz
-@end
--- Pods/Target Support Files/Pods-Muslim QuizTests/Pods-Muslim QuizTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Muslim_QuizTests : NSObject
-@end
-@implementation PodsDummy_Pods_Muslim_QuizTests
-@end
